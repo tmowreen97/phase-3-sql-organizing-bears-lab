0b09e58d168c683125c89930aec6e0317ecebbb4
Tasreen Mowreen <tmowreen97@gmail.com>
2022-11-09 20:41:40 -0500

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,17 @@
+-- Mr. Chocolate
+-- Rowdy
+-- Tabitha
+-- Sergeant Brown
+-- Melissa
+-- Grinch
+-- Wendy
+-- unnamed (refer back to how to create a record that doesn't have one value)
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Mr. Chocolate", 5, "M", "Brown", "Happy", 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Rowdy", 2, "M", "Black", "Angry", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Tabitha", 7, "F", "Grey", "Funny", 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Sergeant Brown", 10, "M", "Brown", "Angry", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Melissa", 4, "F", "Black", "Happy", 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Grinch", 12, "M", "Green", "Grumpy", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Wendy", 6, "F", "Red", "Happy", 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, 4, "F", "White", "Funny", 1);
